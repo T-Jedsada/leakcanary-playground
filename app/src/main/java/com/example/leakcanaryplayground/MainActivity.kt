09bcfae91c5acf9c26bb3e3dab0be496d22b589f
Jedsada Tiwongvorakul <jt@20scoops.net>
2018-04-19 11:36:51 +0700

    test leakcanary

--- app/src/main/java/com/example/leakcanaryplayground/MainActivity.kt
+++ app/src/main/java/com/example/leakcanaryplayground/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.example.leakcanaryplayground
 
-import android.support.v7.app.AppCompatActivity
+import android.annotation.SuppressLint
+import android.os.AsyncTask
 import android.os.Bundle
+import android.os.SystemClock
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        btnTest.setOnClickListener { startAsyncTask() }
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    fun startAsyncTask() {
+        // This async task is an anonymous class and therefore has a hidden reference to the outer
+        // class MainActivity. If the activity gets destroyed before the task finishes (e.g. rotation),
+        // the activity instance will leak.
+        object : AsyncTask<Void, Void, Void>() {
+            override fun doInBackground(vararg params: Void): Void? {
+                // Do some slow work in background
+                SystemClock.sleep(20000)
+                return null
+            }
+        }.execute()
     }
 }
